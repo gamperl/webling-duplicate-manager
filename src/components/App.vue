e68844e3921ea8a677b251c70407a5ad1096868d
Lukas Gamper <lukas.gamper@usystems.ch>
2019-12-27 00:36:39 +0100

    start implementing login logic

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,25 @@
 <template>
-	<Login />
+	<Login v-if="state === 'initialized'" />
+	<div v-else>Loged in ...</div>
 </template>
 
 <script lang="ts">
-import { createComponent } from '@vue/composition-api';
+import { createComponent, watch } from '@vue/composition-api';
 import Login from '@/components/Login.vue';
+import { provideStore } from '@/lib/store';
 
 export default createComponent({
 	components: {
 		Login
+	},
+	setup() {
+		const { state, apikey, domain, persistent } = provideStore();
+		watch(state, () => {
+			console.log(state.value, apikey.value, domain.value, persistent.value);
+		});
+		return {
+			state
+		};
 	}
 });
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,30 +1,28 @@
 <template>
-	<section class="hero is-light is-fullheight">
+	<section class="hero is-dark is-fullheight">
 		<div class="hero-body">
 			<div class="container">
 				<div class="columns is-centered">
 					<div class="column is-5-tablet is-4-desktop is-3-widescreen">
-						<form action="" class="box">
+						<div class="box">
 							<b-field label="Domain">
 								<div class="field-body">
-									<b-input validation-message="Die Domain muss angegeben werden" expanded required />
+									<b-input v-model="domain" validation-message="Die Domain muss angegeben werden" expanded required />
 									<p class="control">
 										<span class="button is-static">.webling.ch</span>
 									</p>
 								</div>
 							</b-field>
 							<b-field label="API Key">
-								<b-input validation-message="Ein API Key hat 32 Zeichen" minlength="32" maxlength="32" required />
+								<b-input v-model="apikey" validation-message="Ein API Key hat 32 Zeichen" minlength="32" maxlength="32" required />
 							</b-field>
 							<b-field>
-								<b-checkbox>Auf diesem Computer merken</b-checkbox>
+								<b-checkbox v-model="persistent">Auf diesem Computer merken</b-checkbox>
 							</b-field>
-							<div class="field">
-								<button class="button is-success">
-									Login
-								</button>
-							</div>
-						</form>
+							<b-field>
+								<b-button type="is-success" @click="submit">Login</b-button>
+							</b-field>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -34,7 +32,21 @@
 
 <script lang="ts">
 import { createComponent } from '@vue/composition-api';
+import { useStore } from '@/lib/store';
 
 export default createComponent({
+	setup() {
+		const { state, domain, apikey, persistent } = useStore();
+		return {
+			domain,
+			apikey,
+			persistent,
+			submit: () => {
+				if (domain.value.length > 0 && apikey.value.length === 32) {
+					state.value = 'authorizing';
+				}
+			}
+		};
+	}
 });
 </script>
