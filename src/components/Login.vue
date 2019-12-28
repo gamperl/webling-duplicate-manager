407f1aaf231a946b6bf3d39d52fd770ee8dbdae8
Lukas Gamper <lukas.gamper@usystems.ch>
2019-12-28 22:08:57 +0100

    improve login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,15 @@
 <template>
-	<section class="hero is-dark is-fullheight">
+	<section class="hero is-light is-fullheight">
 		<div class="hero-body">
 			<div class="container">
 				<div class="columns is-centered">
 					<div class="column is-5-tablet is-4-desktop is-3-widescreen">
 						<div class="box">
+							<b-message v-if="isError" type="is-danger">
+								Domain oder apikey ungültig
+							</b-message>
 							<b-field label="Domain">
-								<div class="field-body">
+								<div class="field has-addons">
 									<b-input v-model="domain" validation-message="Die Domain muss angegeben werden" expanded required />
 									<p class="control">
 										<span class="button is-static">.webling.ch</span>
@@ -14,14 +17,12 @@
 								</div>
 							</b-field>
 							<b-field label="API Key">
-								<b-input v-model="apikey" validation-message="Ein API Key hat 32 Zeichen" minlength="32" maxlength="32" required />
+								<b-input v-model="apikey" validation-message="Ein API Key hat 32 Zeichen" minlength="32" maxlength="32" :has-counter="false" required />
 							</b-field>
 							<b-field>
 								<b-checkbox v-model="persistent">Auf diesem Computer merken</b-checkbox>
 							</b-field>
-							<b-field>
-								<b-button type="is-success" @click="submit">Login</b-button>
-							</b-field>
+							<b-button type="is-primary" @click="submit" :disabled="isLoading">Login</b-button>
 						</div>
 					</div>
 				</div>
@@ -31,7 +32,7 @@
 </template>
 
 <script lang="ts">
-import { createComponent } from '@vue/composition-api';
+import { createComponent, ref } from '@vue/composition-api';
 import { useStore } from '@/lib/store';
 import { useHttp } from '@/lib/http';
 
@@ -39,14 +40,37 @@ export default createComponent({
 	setup() {
 		const { domain, apikey, get } = useHttp();
 		const { state, persistent } = useStore();
+		const isLoading = ref(false);
+		const isError = ref(false);
+
+		domain.value = localStorage.getItem('domain') || '';
+		apikey.value = localStorage.getItem('apikey') || '';
+		persistent.value = domain.value.length > 0 || apikey.value.length > 0;
+
 		return {
 			domain,
 			apikey,
 			persistent,
+			isLoading,
+			isError,
 			submit: async () => {
 				if (domain.value.length > 0 && apikey.value.length === 32) {
-					console.log(await get('config'));
-					state.value = 'authorizing';
+					isLoading.value = true;
+					try {
+						await get('config');
+						isError.value = false;
+						if (persistent.value) {
+							localStorage.setItem('domain', domain.value);
+							localStorage.setItem('apikey', apikey.value);
+						} else {
+							localStorage.removeItem('domain');
+							localStorage.removeItem('apikey');
+						}
+						state.value = 'authorizing';
+					} catch (e) {
+						isLoading.value = false;
+						isError.value = true;
+					}
 				}
 			}
 		};
